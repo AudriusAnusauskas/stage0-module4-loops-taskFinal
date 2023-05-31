61a8c7c6ac33d4a35c9b8d3def31b34ae368b5d1
AudriusAnusauskas <aaaudrius@gmail.com>
2023-05-31 16:03:49 +0300

    finish final task module 4.2 stage0

--- src/main/java/school/mjc/stage0/loops/finalTask/HalfPyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/HalfPyramid.java
@@ -2,7 +2,17 @@ package school.mjc.stage0.loops.finalTask;
 
 public class HalfPyramid {
     public void printHalfPyramid(int cathetusLength) {
-
+        for (int i = 1; i<=cathetusLength; i++ ){
+            for(int j = cathetusLength; j>=1; j--){
+                if(i>=j){
+                    System.out.print("*");
+                } else if (i < j){
+                    System.out.print(" ");
+                }
+                System.out.print("");
+            }
+            System.out.println("");
+        }
     }
 
     public static void main(String[] args) {
--- src/main/java/school/mjc/stage0/loops/finalTask/MultiplicationTable.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/MultiplicationTable.java
@@ -2,5 +2,14 @@ package school.mjc.stage0.loops.finalTask;
 
 public class MultiplicationTable {
     public void printTable(int numberTableToPrint){
+        for(int i = 1; i<=10; i++){
+            System.out.println( i + " x " + numberTableToPrint + " = " + numberTableToPrint*i);
+        }
+
+    }
+
+    public static void main(String[] args) {
+        MultiplicationTable mult = new MultiplicationTable();
+        mult.printTable(4);
     }
 }
